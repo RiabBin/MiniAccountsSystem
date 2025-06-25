512e57eb13ff977d5550cd20a5fb30a90ae950a1
RiabBin <riadbinislam00@gmail.com>
2025-06-25 17:10:40 +0600

    this is my second change

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,8 @@ namespace MiniAccountManagementSystem
             Configuration = configuration;
         }
 
+
+        // this siwhatksk  
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
